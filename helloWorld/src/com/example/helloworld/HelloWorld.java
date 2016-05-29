68afba082a9e1ff4ce7ed0af79e65c0fd2d6796c
michael burney <burney318@gmail.com>
2016-05-29 14:58:24 -0500

    finished helloworld in java 8)

---
+++ helloWorld/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by michaelburney on 5/29/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+}
